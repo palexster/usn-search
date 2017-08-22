2972a2b5bfba0c408bfe4f6027e5f5a7b8c93970
Luke Berner <lukeber4@gmail.com>
2017-08-22 10:53:43 -0300

    Update dump.sh

--- dump.sh
+++ dump.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 mongoexport --host localhost --db cve_ubuntu --collection cve_ubuntu --csv --out cves.out --fields package,os,version,cve,status,priority
-tail -n +2 cves.out > cves.out.tmp && mv cves.out.tmp cves.out
+tail -n +2 cves.out > cves.out.tmp
+rm cves.out
+
+python3 add_quotes.py
